ed631a7521c690e2394de4a7fa88696a3eb36f98
Piotr Litwinski <piotr.litwinski@hotmail.com>
2018-04-22 21:37:48 +0200

    Create lambda package within project directory.

--- Estates.ParserVerifier/build.cmd
+++ Estates.ParserVerifier/build.cmd
@@ -1,2 +1,2 @@
 dotnet restore
-dotnet lambda package --configuration Release --framework netcoreapp2.0 --output-package bin/Release/netcoreapp2.0/Estates.ParserVerifier.zip
+dotnet lambda package --configuration Release --framework netcoreapp2.0 --output-package Estates.ParserVerifier.zip
